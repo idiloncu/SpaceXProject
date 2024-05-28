9ef3c4f2ff8dfd72a6820b6127268db39d95a247
İdil Öncü <oncuidil@gmail.com>
2024-05-28 13:55:03 +0300

    api step1

---
+++ app/src/main/java/com/example/spacexproject/model/SpaceModel.kt
@@ -0,0 +1,7 @@
+package com.example.spacexproject.model
+
+data class SpaceModel(
+    val water_landings:Int,
+    val last_update:String
+)
+
--- app/src/main/java/com/example/spacexproject/view/MainActivity.kt
+++ app/src/main/java/com/example/spacexproject/view/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.spacexproject
+package com.example.spacexproject.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.spacexproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    //https://api.spacexdata.com/v4/capsules
+
 }
